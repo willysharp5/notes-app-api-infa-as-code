af83f7313cca1102affd59124206fe286822bc5a
willysharp10 <edowilliams@gmail.com>
2019-02-12 10:32:56 -0800

    Description update

--- billing.js
+++ billing.js
@@ -5,7 +5,7 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const { storage, source } = JSON.parse(event.body);
   const amount = calculateCost(storage);
-  const description = "Scratch charge";
+  const description = "Note Zulu Charge";
 
   // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
